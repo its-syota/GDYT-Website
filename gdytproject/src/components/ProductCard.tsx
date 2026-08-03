601450bd910af26dfb29430802f2718637b4a252
its-syota <jordancruz231@outlook.com>
2026-08-03 11:28:23 -0400

    we setup the corlor for our price

--- gdytproject/src/components/ProductCard.tsx
+++ gdytproject/src/components/ProductCard.tsx
@@ -6,12 +6,10 @@ function ProductCard() {
         <a href="" className="product-card">
             <div className="product-card-body">
                 <img src={Sample} className="product-card-img"/>
-                <h3 className="product-card-title" style={{color: 'var(--link-text-color)'}}>sample</h3>
-                <p className="product-card-discription" style={{color: 'var(--link-text-color)'}}>this is for placeholder</p>
-                <p className="product-card-price" style={{color: 'var(--link-text-color)'}}>22.67</p>
-            
+                <h3 className="product-card-title">sample</h3>
+                <p className="product-card-description" >this is for placeholder</p>
+                <p className="product-card-price" >19.99</p>
             </div>
-
         </a>
 
     )
